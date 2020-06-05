d8834f1771ae3aa4496138902d9383a744d179fa
Ahemd-M <62062017+Ahemd-M@users.noreply.github.com>
2020-06-05 18:13:49 +0200

    add view price

--- cart.php
+++ cart.php
@@ -19,127 +19,14 @@ session_start();
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-4" id="logo">
-                <a href="home.php" class="logo-text">
-                    Medi<span style="color:#39BAF0; font-size:40px">Store</span>
-                </a>
-            </div>
-            <div class="col-md-2 col-sm-12 col-xs-12" style="display:none " id="navbar_hide">
-                <nav role="navigation" class="navbar navbar-inverse">
-                    <a href="home.php" style="float: left" class="logo-text">
-                        Medi<span style="color:#39BAF0; font-size:40px">Store</span>
-                    </a>
-                    <div id="nav">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="background: #8EBE08; border: none; margin-right: 0">
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                        </div>
-                        <div class="collapse navbar-collapse" id="myNavbar">
-                            <ul class="nav navbar-nav site_nav_menu1">
-                                <li class="first "><a href="home.php">Home</a></li>
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Guarantee</a></li>
-                                <li><a href="#">Disclaimer</a></li>
-                                <li><a href="#">Shipping & Payment</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                                <li><a href="#">Terms & Conditions</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Site Map</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-            <div class="col-md-4 col-md-offset-4 col-sm-offset-2  col-sm-6 col-xs-12">
-                <div id="top_right">
-                    <div id="cart">
-                        <div class="text">
-                            <div class="img">
-                                <a href="cart.html"> <img class="img-responsive" src="images/cart.png" alt="" title="" width="26" height="27" /></a>
-                            </div><span>Your cart:</span><span class="cart_total">€ </span><span class="cart_items"></span>
-                        </div>
-                    </div>
-                    <div id="bottom_right">
-                        <div class="row">
-                            <div class="col-md-6 col-xs-6 wd_auto">
-                                <div class="left">
-                                    <div class="login">
-                                        <a class="btn btn-default reg_button" href="login.html">Login</a>
-                                        <a class="btn btn-default reg_button" href="register.php">Signup</a>
-                                        <a class="btn btn-default reg_button" href="./features/logout_process.php">Logout</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="dropdown-bn wd-33 col-md-6 remove_PL col-xs-6">
-                                <div class="row">
-                                    <div class=" pl-0 col-md-6 col-xs-6">
+    <!-- Header-->
+    <?php
+    if (isset($_SESSION['type']))
+        include('Frames\headerFrameWithoutLogin.php');
+    else
+        include('Frames\headerFrameLoginSignup.html');
+    ?>
 
-                                        <div class="dropdown btn-group">
-
-
-                                        </div>
-                                    </div>
-
-                                    <div class="pl_0 col-md-6 col-xs-6">
-                                        <div class="dropdown btn-group">
-
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
-
-    <div class="container-fluid bg-color">
-        <div class="row">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-xs-12">
-                        <nav role="navigation" class="navbar navbar-inverse" id="nav_show">
-                            <div id="nav">
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                    </button>
-
-                                </div>
-                                <div class="collapse navbar-collapse" id="myNavbar">
-                                    <ul class="nav navbar-nav site_nav_menu1">
-                                        <li class="first "><a href="home.php">Home</a></li>
-                                        <li><a href="#">About us</a></li>
-                                        <li><a href="#">Guarantee</a></li>
-                                        <li><a href="#">Disclaimer</a></li>
-                                        <li><a href="#">Shipping & Payment</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Terms & Conditions</a></li>
-                                        <li><a href="#">Contact Us</a></li>
-                                        <li><a href="#">Site Map</a></li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
     <div class="container">
         <div class="row" id="search_manu" style="margin-top: 10px">
             <div class="col-md-6 col-xs-12">
@@ -269,53 +156,9 @@ session_start();
             </div>
         </div>
 
-
-        <div id="footer1">
-            <div class="container-fluid footer-background">
-                <div class="row">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-2 col-sm-3 col-xs-12 txt-center">
-                                <a href="home.php">
-                                    <span class="logo-text">DRUGSTORE</span>
-                                </a>
-                            </div>
-                            <div class="col-md-7 col-sm-6 col-xs-12">
-                                <div id="footer_menu">
-                                    <a href="home.php">Home</a> |
-                                    <a href="#">About Us</a> |
-                                    <a href="#">Disclaimer</a> |
-                                    <a href="#">Guarantee</a> |
-                                    <a href="#">Shipping & Payment</a> |
-                                    <a href="#">Privacy Policy</a> <br class="disable_content" />
-                                    <a href="#">Terms & Conditions</a> |
-                                    <a href="#">Contact Us</a> |
-                                    <a href="#">Site Map<span></span></a>
-
-                                </div>
-                            </div>
-                            <div class="col-md-3 col-sm-3 col-xs-12">
-                                <div id="social_icons" class="pull-right">
-                                    <a href="#" class="btn btn-default reg_button"><i class="fa fa-facebook"></i></a>
-                                    <a href="#" class="btn btn-default reg_button"><i class="fa fa-twitter"></i></a>
-                                    <a href="#" class="btn btn-default reg_button"><i class="fa fa-yahoo"></i></a>
-                                    <a href="#" class="btn btn-default reg_button"><i class="fa fa-envelope"></i></a>
-                                    <a href="#" class="btn btn-default reg_button"><i class="fa fa-linkedin"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-md-12 col-sm-12 col-xs-12">
-                                <div class="copyright">
-                                    <!--Do not remove Backlink from footer of the template. To remove it you can purchase the Backlink !-->
-                                    © 2017 All right reserved. Designed by
-                                    <a href="http://www.themevault.net/" target="_blank">ThemeVault.</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+    <!--Footer-->
+    <?php include('Frames\footer.html'); ?>
+    
         <a style="display: none" href="javascript:void(0);" class="scrollTop back-to-top" id="back-to-top">
             <i class="fa fa-chevron-up"></i>
         </a>
